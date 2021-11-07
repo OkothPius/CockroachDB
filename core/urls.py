85d8935346f6f499267235562d7d241003c7b77b
Okoth Pius <orukopius8@gmail.com>
2021-11-07 14:14:28 +0300

    feat: add gamelist and gamecreate routes

---
+++ core/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from views import GameListView, GameCreateView
+
+urlpatterns = [
+    path('', GameListView.as_view(), name='home'),
+    path('game/new/', GameCreateView,as_view(), name='game-create'),
+]
