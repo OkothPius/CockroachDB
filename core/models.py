53afa555bc058b2cd283b9ec03cb7d357c469c3c
Okoth Pius <orukopius8@gmail.com>
2021-11-07 14:15:34 +0300

    feat: create game model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    """
+    This is a Game model class.
+    
+    """
+    name = models.CharField(max_length=100)
+    viewer_hour = models.PositiveIntegerField()
+    hours_streamed = models.PositiveIntegerField()
+    acv_num = models.PositiveIntegerField()
+    creators = models.PositiveIntegerField()
+    streams_num = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.name}'
