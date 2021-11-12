7e57e0583f57e5041c2395d232c17989f668c73b
OkothPius <orukopius8@gmail.com>
2021-11-12 18:11:50 +0300

    feat: update

--- game/settings.py
+++ game/settings.py
@@ -4,6 +4,8 @@ import django_heroku
 import dj_database_url
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
